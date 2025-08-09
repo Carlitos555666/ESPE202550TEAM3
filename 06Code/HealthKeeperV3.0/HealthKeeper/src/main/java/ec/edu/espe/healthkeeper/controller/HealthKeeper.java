6b6851f96ddfe563e3bf6d500e448089ba4cbb93
Carlitos555666 <carlitoscarloss6444@gmail.com>
2025-08-08 21:13:18 -0500

    HealthKeeperV2.1

---
+++ 06Code/HealthKeeperV3.0/HealthKeeper/src/main/java/ec/edu/espe/healthkeeper/controller/HealthKeeper.java
@@ -0,0 +1,12 @@
+package ec.edu.espe.healthkeeper.controller;
+
+/**
+ *
+ * @author BugBusters
+ */
+public class HealthKeeper {
+
+    public static void main(String[] args) {
+        System.out.println("Hello World!");
+    }
+}
